27c0ab0d6451adfb6c5fc8556184cddae94d579c
tonyliew95 <39268367+tonyliew95@users.noreply.github.com>
2018-05-15 00:07:03 +0800

    Update ROI.py

--- ROI.py
+++ ROI.py
@@ -19,31 +19,3 @@ def crop(frame):
     cv2.rectangle(frame, (start_col,start_row), (end_col,end_row), (255,0,0), 3)
     
     return (cropped)
-
-
-
-=======
-import cv2
-import numpy as np
-
-def crop(frame):
-    #get height and with of frame
-    height, width = frame.shape[:2]
-
-    #get Region of Interest
-    start_row, start_col = int(height * .25), int(width * .25)
-
-    # Let's get the ending pixel coordinates (bottom right)
-    end_row, end_col = int(height * .75), int(width * .75)
-
-    # Simply use indexing to crop out the rectangle we desire
-    cropped = frame[start_row:end_row , start_col:end_col]
-    #croping the image
-
-    cv2.rectangle(frame, (start_col,start_row), (end_col,end_row), (255,0,0), 3)
-    
-    return (cropped)
-
-
-
->>>>>>> ceda3c94e9f899b85a037487778c8a9092903e6e
