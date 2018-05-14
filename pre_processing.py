86a8c7820679febf328045e2f57009faedacbef0
tonyliew95 <39268367+tonyliew95@users.noreply.github.com>
2018-05-15 00:08:27 +0800

    Update pre_processing.py

--- pre_processing.py
+++ pre_processing.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import cv2
 import numpy as np
 
@@ -19,27 +18,3 @@ def Gfilter(gray_frame):
     #filtering using Gaussian Filtering
     Gfiltering = cv2.GaussianBlur(gray_frame,(5,5),0)
     return(Gfiltering)
-    
-=======
-import cv2
-import numpy as np
-
-#input is image/frame
-
-def resize(frame):
-    #resize input into 0.5 
-    resize_frame = cv2.resize(frame, None , fx = 0.5, fy = 0.5)
-    return(resize_frame)
-
-def grayImage(resize_frame):
-    #grayscale input
-    gray = cv2.cvtColor(resize_frame, cv2.COLOR_BGR2GRAY)
-    return (gray)
-
-def Gfilter(gray_frame):
-    stdX = np.std(gray_frame)
-    #filtering using Gaussian Filtering
-    Gfiltering = cv2.GaussianBlur(gray_frame,(5,5),0)
-    return(Gfiltering)
-    
->>>>>>> ceda3c94e9f899b85a037487778c8a9092903e6e
